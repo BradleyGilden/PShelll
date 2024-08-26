de0c451fb80d4a1b52511c942a34fa76066bd7d2
BradleyGilden <bradleygilden@gmail.com>
2024-08-26 19:12:03 +0200

    conversions

---
+++ Convert.ps1
@@ -0,0 +1,15 @@
+# There are 3 methods of conversion
+$num = 32
+
+# casting
+# There are no safety measures
+Write-Host (([String]$num).GetType())
+
+# Use -as operator, which returns null if it cannot convert
+
+Write-Host (($num -as [String]).GetType())
+$str = 's.d.f'
+Write-Host ($null -eq ($str -as [Int]))
+
+# Use .NET methods
+Write-Host ($num.ToString().GetType())
