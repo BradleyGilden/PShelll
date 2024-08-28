bd4ae9742655ae38630d16c489dfe321fa434713
BradleyGilden <bradleygilden@gmail.com>
2024-08-28 21:34:11 +0200

    update custom prompt

--- CustomPrompt.ps1
+++ CustomPrompt.ps1
@@ -8,18 +8,12 @@ if($CWD -cmatch "C:\\Users\\$($Env:USERNAME)") {
     $CWD1 = $CWD[0..($CWD.Length - 3)]
     $i = $CWD1.Length - 1
     while($i -ge 0) {
-        if (-not $CWD1[$i]) {
-            Remove-Variable -Name $CWD1[$i]
-            $i--
-        }
-        else {
-            $CWD1[$i] = $CWD1[$i][0]
-        }
-        $i--
+        $CWD1[$i--] = $CWD1[$i][0]
     }
-    Write-Host $CWD1[0]
+    $CWD1 = Write-Output  | Where-Object {$_}
     $CWD2 = $CWD[-2..-1]
     $CWD = $CWD1 + $CWD2
+    $CWD = $CWD -join '\'
     Write-Host $CWD, $CWD1, $CWD2
-    Write-Host ('~' + $CWD)
+    Write-Host ('~\' + $CWD)
 }
