d8dc077592bf510eb1d088fcd92a4bffc356a1d2
BradleyGilden <bradleygilden@gmail.com>
2024-09-03 20:49:17 +0200

    altering custom prompt

--- CustomPrompt.ps1
+++ CustomPrompt.ps1
@@ -1,35 +1,3 @@
-$CWD = $PWD.Path
 
-Write-Host $CWD
-
-if($CWD -cmatch "C:\\Users\\$($Env:USERNAME)") {
-    $CWD = $CWD -creplace "C:\\Users\\$($Env:USERNAME)", ""
-    $CWD = $CWD -split '\\'
-    $CWD1 = $CWD[0..($CWD.Length - 3)]
-    $i = $CWD1.Length - 1
-    while($i -ge 0) {
-        $CWD1[$i--] = $CWD1[$i][0]
-    }
-    $CWD1 = Write-Output $CWD1 | Where-Object {$_}
-    if ($CWD1 -isnot [array]) {
-        $CWD1 = @($CWD1)
-    }
-    $CWD2 = $CWD[-2..-1]
-    $CWD = $CWD1 + $CWD2
-    $CWD = $CWD -join '\'
-    if ($CWD) {
-        $CWD = '~\' + $CWD
-    } else {
-        $CWD = '~'
-    }
-
-    $Env:CWD = $CWD
-} elseif ($CWD.StartsWith("[A-Z]:\\Users")){
-    $CWD = $CWD -creplace "C:\\", ""
-    $CWD = $CWD -split '\\'
-    $CWD = $CWD -join '\'
-    $CWD = '\' + $CWD
-    $Env:CWD = $CWD
-} else {
-    $Env:CWD = $CWD
-}
+# Invokes method before prompt
+# Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -Action {CustomPrompt}
