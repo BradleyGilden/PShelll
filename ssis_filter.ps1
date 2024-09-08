51b58a5a3a51b93aadb33c5c5fa341ae95b35de0
BradleyGilden <bradleygilden@gmail.com>
2024-09-08 20:24:44 +0200

    update powershell script

--- ssis_filter.ps1
+++ ssis_filter.ps1
@@ -1,37 +1,48 @@
 param(
+    # File Path (to be hard coded)
     [String]$FilePath,
+
+    # Date of log file
+    [Parameter(Mandatory = $true)]
     [String]$LogDate,
+
+    # Last N lines of a log file
     [Int]$Lines=1000
 )
 
-function DisplayError {
+<#
+Function: Write-ErrorMessage
+Purpose: Writes Error Message
+Returns: $null
+#>
+function Write-ErrorMessage {
     Write-Host -ForegroundColor Red "Please ensure your date argument format is as follows:"
     Write-Host -ForegroundColor Magenta 'dd-mm-yyyy' -NoNewline
     Write-Host " OR " -NoNewline
     Write-Host -ForegroundColor Magenta 'dd/mm/yyyy' -NoNewline
     Write-Host " OR " -NoNewline
     Write-Host -ForegroundColor Magenta 'dd\mm\yyyy'
+    exit 1
 }
 
 $dateParams = $LogDate -csplit "[-\\/]"
 
 $dateParams = $dateParams | Where-Object {$_}
 
+# check for correct date format
 if ($dateParams.Length -lt 3) {
-    DisplayError
-    exit
+    Write-ErrorMessage
 } elseif ($dateParams[0].Length -ne 2 -or $dateParams[1].Length -ne 2 -or $dateParams[2].Length -ne 4) {
-    DisplayError
-    exit
+    Write-ErrorMessage
 } elseif ($dateParams[0] -notmatch "^\d+$" -or $dateParams[1] -notmatch "^\d+$" -or $dateParams[2] -notmatch "^\d+$") {
-    DisplayError
-    exit
+    Write-ErrorMessage
 }
 
 $day = $dateParams[0]
 $month = $dateParams[1]
 $year = $dateParams[2]
 
+# Match regex file pattern to text content in file
 $filePattern = "$day/$month/$year\s\d\d:\d\d:\d\d:\sExecuting\s<[a-zA-Z]:\\MrpSports\\scripts\\(pre)?stock\\ld_(res|pre)_stock.bat>\s\.\.\."
 
 try {
@@ -43,7 +54,7 @@ try {
     
     if (-not $matched) {
         Write-Host -ForegroundColor Red "No log entries found for the given date $day/$month/$year"
-        exit
+        exit 1
     }
 
     foreach($match in $matched) {
@@ -58,5 +69,5 @@ try {
     } else {
         Write-Host -ForegroundColor Red $_.Exception.Message
     }
-    exit
+    exit 1
 }
